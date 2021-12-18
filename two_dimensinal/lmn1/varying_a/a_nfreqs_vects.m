8966c350e4a6a9a0e83be8e624816afcc5ed5982
htnabe <n21m312@matsu.shimane-u.ac.jp>
2021-12-18 15:46:51 +0900

    convert comment to ENG

--- two_dimensinal/lmn1/varying_a/a_nfreqs_vects.m
+++ two_dimensinal/lmn1/varying_a/a_nfreqs_vects.m
@@ -2,12 +2,12 @@
 clear;
 disp ("Start")
 
-b = 0.500; c = 0.050; # 幅b，厚さc
-r = 1.52*10^3; # 密度
-E_xx = 148*10^9; E_yy = 10.50*10^9; # 繊維方向弾性率，繊維垂直方向弾性率
-E_zz = E_yy; # 二次元直交異方性材料の場合の仮定
-n_xy = 0.30; n_yz = 0.59; n_xz = 0.30; # ポアソン比
-n_yx = (E_yy * n_xy)/E_xx; # 推測されるポアソン比
+b = 0.500; c = 0.050; # width b, thickness c
+r = 1.52*10^3; # density
+E_xx = 148*10^9; E_yy = 10.50*10^9; # axial + vertical young's modulus
+E_zz = E_yy; # two dimensinal orthotropy's condition
+n_xy = 0.30; n_yz = 0.59; n_xz = 0.30; # poisson's ratio
+n_yx = (E_yy * n_xy)/E_xx; # two dimensinal orthotropy's condition
 n_zy = E_zz * n_yz/E_yy;
 n_zx = E_zz * n_xz/E_xx;
 
@@ -15,11 +15,11 @@ for array = [n_yx n_zy n_zx]
   test_poisson(array);
 end
 
-Q_xyxy = 5.61*10^9; Q_yzyz = 3.17*10^9; # せん断弾性係数
-Q_xzxz = Q_xyxy; # 二次元直交異方性材料の場合の仮定
-l = m = n = 1; # モード次数
+Q_xyxy = 5.61*10^9; Q_yzyz = 3.17*10^9; # 縺帙ｓ譁ｭ蠑ｾ諤ｧ菫よ焚
+Q_xzxz = Q_xyxy; # two dimensinal orthotropy's condition
+l = m = n = 1; # mode degree
 
-test_variables(b, c, r, E_xx, E_yy, E_zz, n_xy, n_yz, n_xz, Q_xyxy, Q_yzyz, Q_xzxz);
+test_variables(a, c, r, E_xx, E_yy, E_zz, n_xy, n_yz, n_xz, Q_xyxy, Q_yzyz, Q_xzxz);
 
 Delta = 1 - n_xy * n_yx - n_xz * n_zx - n_yz * n_zy - n_xy * n_yz * n_zx - n_xz * n_yx * n_zy;
 Q_xxxx = E_xx * ((1 - n_yz * n_zy)/Delta);
@@ -29,7 +29,7 @@ Q_yyyy = E_yy * ((1 - n_xz * n_zx)/Delta);
 Q_yyzz = E_yy * ((n_zy + n_xy * n_zx)/Delta);
 Q_zzzz = E_zz * ((1 - n_xy * n_yx)/Delta);
 
-# for分に関係ない部分
+# havenothing with iteration
 N_11_term = Q_xyxy * ((m*pi)/b)^2 + Q_xzxz * ((n*pi)/c)^2;
 N_22_term = Q_yzyz * ((n*pi)/c)^2 + Q_yyyy * ((m*pi)/b)^2;
 N_33_term = Q_zzzz * ((n*pi)/c)^2 + Q_yzyz * ((m*pi)/b)^2;
@@ -49,7 +49,7 @@ for cnt = 1:14001
   N = [N_11, N_12, N_13; N_21, N_22, N_23; N_31, N_32, N_33];
   [V(:,:,cnt), LAMBDA(:,:,cnt)] = eig (N);
 
-  # 各ベクトルの第２成分（y軸方向成分）を常に正にする
+  # make one of an eigen vector's element plus
   if (V(:,1,cnt)(2) >0),
     N_vec_1(:,cnt) = V(:,1,cnt);
   else
@@ -66,7 +66,6 @@ for cnt = 1:14001
     N_vec_3(:,cnt) = -V(:,3,cnt);
   endif
 
-  # 固有値が正なら固有角振動数が存在する
   if (LAMBDA(2,2,cnt)>0),
     N_freq_2(cnt) = sqrt(LAMBDA(2,2,cnt)/r);
   else
@@ -85,7 +84,7 @@ for cnt = 1:14001
   cnt++;
 endfor
 
-# 固有角振動数, 固有ベクトルの書き込み
+# write data on files
 write_naf(cnt, measure, N_freq_1, 1);
 write_naf(cnt, measure, N_freq_2, 2);
 write_naf(cnt, measure, N_freq_3, 3);
